4ac6d5a511c909682bacee4b87551ef1d623310e
Conzi84 <bandwurmcosti@gmail.com>
2025-08-26 11:44:52 +0200

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,1 +1,10 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: ['./index.html', './src/**/*.{ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config
 
